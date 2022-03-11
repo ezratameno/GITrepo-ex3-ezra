3de08c9c74dcc37d360350a9f9e15e9a30846346
ezratameno <ezratameno@gmail.com>
2022-03-11 14:09:59 +0200

    dfdf

--- first_java.java
+++ first_java.java
@@ -1,4 +1,4 @@
-public class First_java {
+public class first_java {
    public static void main(String []args) {
       for (int i=1; i < 10; i++) {
          System.out.println("My name is ezra1 !"); 
