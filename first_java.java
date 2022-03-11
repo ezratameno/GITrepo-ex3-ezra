0aa8127643c6db4dd1729d5e4d044c244085141c
ezratameno <ezratameno@gmail.com>
2022-03-11 14:13:29 +0200

    change to ezra2

--- first_java.java
+++ first_java.java
@@ -1,7 +1,7 @@
 public class first_java {
    public static void main(String []args) {
       for (int i=1; i < 10; i++) {
-         System.out.println("My name is ezra1 !"); 
+         System.out.println("My name is ezra2 !"); 
       }
    }
 }
